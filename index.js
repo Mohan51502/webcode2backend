140cefd7ca9dca8cb7aad21d2b0641dea7340dcb
Mohan51502 <rajmohan51502@gmail.com>
2023-05-25 06:28:20 +0530

    second

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const app = require("./app");
 
 const node_server = express();
 require('dotenv').config();
-var cors = require('cors');
+// const cors = require("cors");
 
 
 
 require("./dbconfig");
-
-node_server.use(cors());
+// node_server.use(
+//     cors({
+//       origin: "*",
+//     })
+//   );
 
 node_server.use("/", app);
 const port = process.env.PORT || 5000;
